05a3042f211be3973c6336781bcc6b9131a8a37f
Andrew Joubert <andrewhenrijoubert@gmail.com>
2016-05-15 17:28:21 +0100

    Added humidity sensor to the sensors and added it to be called by the mission list for testing. o

---
+++ library/sensors/SenseHumidity.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from sense_hat import SenseHat
+#add the project folder to pythpath
+sys.path.append('../../')
+
+from library.components.SensorModule import SensorModule as Sensor
+from library.components.MetaData import MetaData as MetaData
+
+
+class SenseHumidity(Sensor):
+
+    def __init__(self):
+        super(SenseHumidity, self).__init__()
+        tempMetaData = MetaData('Humidity')
+        tempMetaData.setValueCallback(self.getHumidityValue)
+        tempMetaData.setUnitCallback(self.getHumidtyUnit)
+        self.addMetaData(tempMetaData)
+
+    def getHumidityValue(self):
+        sense = SenseHat()
+        temp = sense.pressure()
+        return str(temp)
+
+    def getHumidtyUnit(self):
+        return " %"
+
+    def getMetaData(self):
+        return super(SenseHumidity, self).getMetaData()
